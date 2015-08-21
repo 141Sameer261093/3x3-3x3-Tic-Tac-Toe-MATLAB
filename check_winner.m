f2cdf9cd183ada820a6a9710f09283989f4b33dd
Sameer Shah <shah.sameer1993@yahoo.com>
2015-08-21 18:07:01 +0530

    added draw case

--- check_winner.m
+++ check_winner.m
@@ -1,15 +1,17 @@
-function [x] = check_winner(m)
+function [winner] = check_winner(m)
 
-x = 0;
+winner = 0;
+countZeros = 0;
+
     for i = 1:3
     
       m1 = m(i,:);
       
       if( sum(m1) == 27 )
-           x = 1;  
+           winner = 1;  
       end
       if( sum(m1) == 3 ) 
-           x = 1;  
+           winner = 1;  
       end
       
     end
@@ -19,28 +21,39 @@ x = 0;
         m2 = m(:,j);
       
       if( sum(m2) == 27 )
-           x = 1;  
+           winner = 1;  
       end
       if( sum(m2) == 3 ) 
-           x = 1;  
+           winner = 1;  
       end
       
     end
     
     if m(1,1) + m(2,2) + m(3,3) == 27
-        x = 1;
+        winner = 1;
     end
     if m(1,1) + m(2,2) + m(3,3) == 3
-        x = 1;
+        winner = 1;
     end
     
     if m(3,1) + m(2,2) + m(1,3) == 27
-        x = 1;
+        winner = 1;
     end
     if m(3,1) + m(2,2) + m(1,3) == 3
-        x = 1;
+        winner = 1;
     end
     
+    for i = 1:3
+        for j = 1:3
+            if m(i,j) == 0
+                countZeros = countZeros + 1;
+            end
+        end
+    end
     
+    if (countZeros == 0)
+        winner = 3;
+    end
     
+        
 end
